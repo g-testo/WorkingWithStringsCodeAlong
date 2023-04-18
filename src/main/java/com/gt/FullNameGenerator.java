02f377e2bde54adc49744d5d9688904815813ef5
Gregory Testo <gt@gregorytesto.com>
2023-04-18 09:30:26 -0400

    WIP: Creating full name generator

---
+++ src/main/java/com/gt/FullNameGenerator.java
@@ -0,0 +1,43 @@
+package com.gt;
+// Import scanner
+import java.util.Scanner;
+//import java.util.*;
+public class FullNameGenerator {
+    public static void main(String[] arg){
+        // Initialize Scanner
+        Scanner userScanner = new Scanner(System.in);
+
+        // Create a variable to store the full name,
+        // initialize as empty String
+        String fullName = "";
+
+        System.out.println("Please enter your name");
+
+        // Prompt user for first name, Make sure to trim
+        System.out.print("First name: ");
+        String firstName = userScanner.nextLine().trim();
+
+        // Prompt user for middle name "", Make sure to trim
+        System.out.print("Middle name: ");
+        String middleName = userScanner.nextLine().trim();
+
+        // Prompt user for last name, Make sure to trim
+        System.out.print("Last name: ");
+        String lastName = userScanner.nextLine().trim();
+
+        // Prompt user for suffix, Make sure to trim
+        System.out.print("Suffix: ");
+        String suffix = userScanner.nextLine().trim();
+
+        // Add first name to fullNameVariable
+        // * Add Middle name to fullNameVariable, Making sure to have whitespace
+        // Add last name to fullNameVariable
+
+        // if there is a suffix, add (", " + suffix) to fullNameVariable
+        // Otherwise do nothing
+
+        // Print out full name
+
+    }
+
+}
--- src/main/java/com/gt/WorkingWithString.java
+++ src/main/java/com/gt/WorkingWithString.java
@@ -1,7 +1,79 @@
 package com.gt;
+import java.util.Arrays;
 
+import javax.sound.midi.SysexMessage;
 public class WorkingWithString {
     public static void main(String[] args){
-        System.out.println("My superpower is: Magneto");
+//        String word1 = "Hello ";
+//        String word2 = "World!";
+//        String result = word1.concat(word2);
+//
+//        int wordLength = result.length();
+//        System.out.println((wordLength));
+        ////////////
+//        char myChar = 'G';
+//        String myName = "Greg";
+//
+//        String phrase = "Big tex says\n \"Hello\"! test";
+//        System.out.println(phrase);
+
+        /////////////
+//        int num1 = 10;
+//        int num2 = 10;
+//        System.out.println(num1 == num2);
+
+//        String phrase1 = new String("Hello World"); // 234g23h
+//        String phrase2 = new String("Hello World"); // sdc454d
+
+//        System.out.println(phrase1 == phrase2); // A
+//        System.out.println(phrase1.equals(phrase2)); // B
+
+        // Primitives: int long short boolean char double float // Compare with ==
+        // Non-primitive: String // Compare .equals
+
+        ///////////
+
+        //        String myName = "Salha";
+        //
+        //        String myUpperCaseName = myName.toUpperCase();
+        //
+        //        System.out.println(myUpperCaseName);
+
+        //////////
+//        String phrase = "                   Hello World    !  ";
+//        System.out.println(phrase.trim());
+
+//        String myPhrase = "Braynel is so awesome!!!!!";
+//        System.out.println(myPhrase.startsWith("bray"));
+
+        ////////////
+
+        String trackingCode = "USA-12981-Y-22";
+        char hasShipped = trackingCode.charAt(1);
+        System.out.println(hasShipped);
+
+        ///////////////
+
+//        String productCode1 = "ACME-12213"; // "ACME", "12213"
+//        int dashPosition1 = productCode1.indexOf("-"); // 4
+//        String vendor1 = productCode1.substring(0, dashPosition1); // "ACME"
+//        String productNum1 = productCode1.substring(dashPosition1 + 1); // "12213"
+//        System.out.println("Vendor1: " + vendor1 + " ProductNum1: " + productNum1);
+//
+//
+//        String productCode2 = "SAMSUNG-320233242"; // "SAMSUNG", "320233242"
+//        int dashPosition2 = productCode2.indexOf("-"); // 7
+//        String vendor2 = productCode2.substring(0, dashPosition2); // "SAMSUNG"
+//        String productNum2 = productCode2.substring(dashPosition2+1); // "320233242"
+//        System.out.println("Vendor2: " + vendor2 + " ProductNum2: " + productNum2);
+
+
+        ////////////
+
+        String str = "my@dear@aunt@sally";
+//        System.out.println(str.substring(3, 7));
+        System.out.println(Arrays.toString(str.split("@")));
     }
+
+
 }
