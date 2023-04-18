4ae3402b506a50b8b57f8d0fe31c42e432500c92
Gregory Testo <gt@gregorytesto.com>
2023-04-18 14:38:39 -0400

    finished covering String builder

---
+++ src/main/java/com/gt/StringBuilding.java
@@ -0,0 +1,24 @@
+package com.gt;
+
+public class StringBuilding {
+    public static void main(String[] args){
+//        String word1 = "Hello ";
+//        String word2 = "world";
+//
+//        String combined = word1 + word2;
+
+        /////////
+
+//        StringBuilder skills = new StringBuilder();
+//
+//        skills.append("Git ");
+//        skills.append("");
+//
+//        skills.insert(2, "333");
+//
+//        System.out.println(skills);
+
+
+    }
+
+}
